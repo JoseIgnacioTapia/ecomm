71c00f87c0f5a3e63113648613f2aea5db6a1dda
José Ignacio <josignaciot@gmail.com>
2020-11-05 16:24:25 -0300

    Mutli-Part Forms

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -14,9 +14,12 @@ router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle], [requirePrice], (req, res) => {
+router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
   const errors = validationResult(req);
-  console.log(errors);
+  
+  req.on('data', data => {
+    console.log(data.toString());
+  });
 
   res.send('submitted');
 })
